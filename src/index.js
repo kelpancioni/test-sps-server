b1c2ae239889b3be817ab1c48da1fb9dcd46f079
Kelsey Pancioni <kel.kelsey@gmail.com>
2026-03-11 20:09:30 +0000

    feat: setup project structure and dependencies

--- src/index.js
+++ src/index.js
@@ -1,13 +1,26 @@
-const express = require("express");
-const routes = require("./routes");
-const cors = require("cors");
+require('dotenv').config();
+
+const express = require('express');
+const cors = require('cors');
+const routes = require('./routes');
 
 const app = express();
 
-app.use(cors());
+const PORT = process.env.PORT || 3001;
 
+// Middlewares
+app.use(cors());
+app.use(express.json());
+
+// Rotas
 app.use(routes);
 
-app.listen(process.env.PORT, () => {
-  console.log("Server is running on http://localhost:3000");
+// Tratamento de erro genérico
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).json({ error: 'Erro interno do servidor' });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
 });
